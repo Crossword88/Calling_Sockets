62e5f5592ba2ece446a52e98239dc728fb985fb2
Crossword88 <shmakovsergej@gmail.com>
2024-01-19 04:56:18 +0100

    loaded

---
+++ Client/ClientIncluding.h
@@ -0,0 +1,11 @@
+#pragma once
+
+#pragma comment(lib, "ws2_32.lib")
+#pragma comment(lib,"winmm.lib") 
+#include <winsock2.h>
+#include <WS2tcpip.h>
+#include <iostream>
+
+#include <windows.h>
+#include <opencv2/opencv.hpp>
+#include <stdio.h>
---
+++ Server/ServerIncluding.h
@@ -0,0 +1,12 @@
+#pragma once
+
+#pragma comment(lib, "ws2_32.lib")
+#pragma comment(lib,"winmm.lib") 
+#include <winsock2.h>
+#include <WS2tcpip.h>
+
+#include <windows.h>
+#include <opencv2/opencv.hpp>
+#include <stdio.h>
+#include <wininet.h>
+#pragma comment(lib, "wininet.lib")
